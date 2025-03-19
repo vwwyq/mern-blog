4f6e6f853ca9b43df0c209044e95bd9d02139007
vwwyq <siyonaj7@gmail.com>
2025-03-19 21:17:52 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
+//import Header from './components/Header';
+//import Footer from './components/Footer';
+//import PrivateRoute from './components/PrivateRoute';
+//import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+//import CreatePost from './pages/CreatePost';
+//import UpdatePost from './pages/UpdatePost';
+//import PostPage from './pages/PostPage';
+//import ScrollToTop from './components/ScrollToTop';
+//import Search from './pages/Search';
 
 export default function App() {
   return (
-    <h1 className='text-3xl'>
-      Hello world  
-    </h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
