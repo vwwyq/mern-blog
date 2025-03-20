65220b69c280fbb22f2e77f9843d8630200ac2fa
vwwyq <siyonaj7@gmail.com>
2025-03-21 01:22:04 +0530

    changed client files

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,18 @@
-/** @type {import('tailwindcss').Config} */
+import flowbitePlugin from 'flowbite/plugin';
+import tailwindScrollbar from 'tailwind-scrollbar';
+
+  /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      './index.html',
-      './src/**/*.{js,ts,jsx,tsx}',
-      'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-    ],
-    theme: {
-      extend: {},
-    },
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
     plugins: [
-      require('flowbite/plugin'),
-      //require('tailwind-scrollbar'),
+      flowbitePlugin,
+      tailwindScrollbar,
     ],
-  };
+};
