cc8a8db0e69d1f337a56fa275055fdd5a80dc394
vwwyq <siyonaj7@gmail.com>
2025-03-22 00:10:13 +0530

    update user profile page functionality.

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,34 +1,94 @@
-import { Button, TextInput } from 'flowbite-react';
-import { useSelector } from 'react-redux';
+import { Alert, Button, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateStart, updateSuccess, updateFailure } from '../redux/user/userSlice';
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+  const [updateUserError, setUpdateUserError] = useState(null);
+  const [formData, setFormData] = useState({});
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setUpdateUserError(null);
+    setUpdateUserSuccess(null);
+
+    if (Object.keys(formData).length === 0) {
+      setUpdateUserError('No changes made');
+      return;
+    }
+
+    try {
+      dispatch(updateStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(updateFailure(data.message));
+        setUpdateUserError(data.message);
+      } else {
+        dispatch(updateSuccess(data));
+        setUpdateUserSuccess("User's profile updated successfully");
+      }
+    } catch (error) {
+      dispatch(updateFailure(error.message));
+      setUpdateUserError(error.message);
+    }
+  };
 
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <TextInput
           type='text'
           id='username'
           placeholder='Username'
           defaultValue={currentUser?.username || ''}
+          onChange={handleChange}
         />
         <TextInput
           type='email'
           id='email'
           placeholder='Email'
           defaultValue={currentUser?.email || ''}
+          onChange={handleChange}
         />
-        <TextInput type='password' id='password' placeholder='New Password' />
+        <TextInput
+          type='password'
+          id='password'
+          placeholder='New Password'
+          onChange={handleChange}
+        />
         <Button type='submit' gradientDuoTone='purpleToBlue' outline>
           Update
         </Button>
       </form>
-      <div className="text-red-500 flex justify-between mt-5">
+      <div className='text-red-500 flex justify-between mt-5'>
         <span className='cursor-pointer'>Delete Account</span>
         <span className='cursor-pointer'>Sign Out</span>
       </div>
+      {updateUserSuccess && (
+        <Alert color='success' className='mt-5'>
+          {updateUserSuccess}
+        </Alert>
+      )}
+      {updateUserError && (
+        <Alert color='failure' className='mt-5'>
+          {updateUserError}
+        </Alert>
+      )}
     </div>
   );
 }
